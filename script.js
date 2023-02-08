4f453986b0eb6689a8eba71dea72c92e7df556f6
BMcCarthy96 <86472500+BMcCarthy96@users.noreply.github.com>
2023-02-08 14:03:55 -0600

    Fixed game start to finish in scirpt.js file

--- script.js
+++ script.js
@@ -1,3 +1,37 @@
-let ComputerInput = function getComputerChoice {
+//Starts game with zero points for both user and computer
+const game = () => {
+    let playerScore = 0;
+    let computerScore = 0;
+}
+
+//This function selects rock, paper, or scissors for the computer
+let computerInput = function getComputerChoice {
 
 }
+
+//This function selects rock, paper, or scissors for the player
+let userInput = function getUserChoice {
+
+}
+
+//This function compares userInput to computerInput to select winner
+let winner = function getWinningChoice {
+if (userInput === computerInput) {
+    console.log("It's a tie!");
+} else if (userInput === "rock" && computerInput === "paper") {
+    console.log("Computer wins!") 
+} else if ()
+
+}
+
+//Edit later
+let userInput = onclick;
+
+let winner = (userInput === computerInput) ? "It's a tie" :
+  (userInput === "rock" && computerInput === "paper") ? "Computer wins!" :
+  (userInput === "rock" && computerInput === "scissors") ? "You win!" :
+  (userInput === "paper" && computerInput === "scissors") ? "Computer wins!" :
+  (userInput === "paper" && computerInput === "rock") ? "You win!" :
+  (userInput === "scissors" && computerInput === "rock") ? "Computer wins!" :
+  (userInput === "scissors" && computerInput === "paper") ? "You win!" :
+  "Something went wrong, uh oh!";
